e155fcd4139ada578f3da4366334e4dd96baabe0
jihoon hyeon <97426920+hyeonjh@users.noreply.github.com>
2022-02-14 14:20:42 +0900

    first

---
+++ src/main/java/com/sparta/gongguri2/Gongguri2Application.java
@@ -0,0 +1,13 @@
+package com.sparta.gongguri2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gongguri2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gongguri2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/gongguri2/Gongguri2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.gongguri2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gongguri2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
